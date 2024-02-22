ffa6c447f486adedfb9a8a33753da0c1078c3bdd
Bastien Chevriaut <bastien.chevriaut-ext@pole-emploi.fr>
2024-02-22 09:59:53 +0100

    developpement avec apis

--- src/main/java/com/example/stock/web/StockController.java
+++ src/main/java/com/example/stock/web/StockController.java
@@ -2,10 +2,7 @@ package com.example.stock.web;
 
 import com.example.stock.domain.StockManager;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StockController {
@@ -18,19 +15,13 @@ public class StockController {
 
     @RequestMapping("product")
     @PostMapping
-    public ResponseEntity<Boolean> orderProduct(@RequestParam("id") int id,
-                                                @RequestParam("name") String name,
-                                                @RequestParam("quantity") int quantity) {
-
-        ProductDTO productDTO = new ProductDTO(id, name, quantity);
+    public ResponseEntity<Boolean> orderProduct(@RequestBody ProductDTO productDTO) {
 
         if (stockManager.isAvailiable(ProductDTO.to(productDTO))) {
             return ResponseEntity.ok(true);
         } else {
             return ResponseEntity.ok(false);
         }
-
-
 
     }
 
